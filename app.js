7075bf793be8c36e09da0d33dbbf7159b4557f80
Bambo Bello <user@Bambo.local>
2023-11-05 22:39:45 +0100

    JOI VALIDATIONS

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const AuthRoute = require('./Routes/Auth.route')
 
 const app = express()
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', async (req, res, next) => {
     res.send("Hello World")
